5895842cab335f222fdb5a8671c27a33d29ebcef
lorenzomoulin <lorenzomoulin@gmail.com>
2018-09-07 20:56:26 -0300

    Add files via upload

--- Trab1/src/main.c
+++ Trab1/src/main.c
@@ -1,11 +1,16 @@
 #include "utils.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include "io.h"
 
 int main(int argc, char** argv){
     
     int N = atoi(argv[1]);
-    float x[N], y[N];
+    int num_arestas = N*(N-1)/2;
+    float* x;
+    float* y;
+    x = malloc(N*sizeof(*x));
+    y = malloc(N*sizeof(*y));
     for (int i = 0; i < N; i++)
         scanf("%f %f\n", &x[i], &y[i]);
     printf("vetor x: ");
@@ -15,6 +20,22 @@ int main(int argc, char** argv){
     for (int i = 0; i < N; i++)
         printf("%f ", y[i]);
     
-    int** d = gera_distancias(x, y, N);
+    printf("\n");
+    //aresta* d = gera_distancias(x, y, N);
+
+    /*for (int i = 0; i < num_arestas; i++){
+        printf("Aresta %d %d: %d\n", retorna_i(d,i), retorna_j(d,i), retorna_peso(d,i));
+        
+    }*/
+    printf("\n");
+    //quicksort(d, 0, num_arestas);
+    aresta* d = kruskal(x, y, N);
+    for (int i = 0; i < N-1; i++){
+        printf("Aresta %d %d: %d\n", retorna_i(d,i), retorna_j(d,i), retorna_peso(d,i));
+        
+    }
+    
+    gera_mst(d, N, "berlin52");
+    
     return 0;
 }
