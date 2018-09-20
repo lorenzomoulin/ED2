bcbdafcd3dbfd50d9db227c6f42bbc4a0bf4b988
lorenzomoulin <lorenzomoulin@gmail.com>
2018-09-20 18:12:41 -0300

    Update utils.h

--- Trab1/utils.h
+++ Trab1/utils.h
@@ -13,18 +13,20 @@ typedef struct aresta aresta;
 */
 int euclideana(float* x, float* y, int i, int j);
 
-/* Gera matriz simetrica com as distancias de todas as cidades considerando que d(i,j) = d(j,i)
+/* Gera vetor de aresta com as distancias de todas as cidades considerando que d(i,j) = d(j,i)
 *inputs: vetores de coordenadas x e y e quantidade de cidades
-*output: matriz com todas as distancias
+*output: vetor com todas as distancias
 *pre-condicao: os vetores devem possuir pelo menos 2 elementos
-*pos-condicao: matriz de inteiros devidamente alocada com N*(N-1)/2 elementos
+*pos-condicao: vetor de inteiros devidamente alocado com N*(N-1)/2 elementos
 */
 aresta* gera_distancias(float* x, float* y, int N);
 
-/**/
-//bool compara_aresta(aresta a, aresta b)
-
-/**/
+/* Ordena vetor de arestas com relacao ao peso
+*inputs: vetor de arestas e posicoes inicial e final
+*output: nenhum
+*pre-condicao: vetor nao vazio, 0 <= inicio <=fim
+*pos-condicao: vetor ordenado
+*/
 void quicksort(aresta* arestas, int inicio, int fim);
 
 int retorna_peso(aresta* a, int i);
@@ -33,12 +35,24 @@ int retorna_i(aresta* a, int i);
 
 int retorna_j(aresta* a, int i);
 
+/* Gera vetor de arestas que compoem a MST
+*inputs: numero de vertices (N), vetor de arestas com todas as arestas do grafo
+*output: vetor de arestas da MST
+*pre-condicao: N > 0 e d nao vazio e ordenado por peso
+*pos-condicao: vetor de arestas alocado
+*/
 aresta* kruskal(int N, aresta* d);
 
+/*Gera vetor de vertices que fazem parte do tour (sem repeticao)
+*inputs: vetor de arestas da MST e numero de vertices
+*output: vetor de vertices do tour
+*pre-condicao: mst nao vazia e de tamanho N - 1, N > 0
+*pos-condicao: vetor de int alocado e de tamanho N
+*/
 int* caminha(aresta* mst, int N);
-
+/* Funcao que caminha recursivamente em vetor de lista de vertices chamada dentro da caminha
+*inputs: vetor de lista de vertices, 
+*/
 void tour(lista** adj, int pos, int* listatour, int* tamanho, bool* visitados);
-
-//int* tour_otimo(aresta* a, int N);
 
 #endif
